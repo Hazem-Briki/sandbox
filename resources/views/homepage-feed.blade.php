cbda0fc4294115b7b057acf2622aabfb98160270
Hazem Briki <hazem.briki@loreal.com>
2024-07-02 16:37:19 +0200

    adding JQUERY script

--- resources/views/homepage-feed.blade.php
+++ resources/views/homepage-feed.blade.php
@@ -1,46 +1,68 @@
 <x-layout>
-    <div class="container py-md-5 container--narrow">
-
+    <div class="container-object">
         @foreach ($exams as $exam)
-            <div class="question-discussion-header">{{ $exam->question_number }}<br></div>
-            <div class="question-body mt-3 pt-3">
-                <p class="card-text">{!! str_replace('(//img url)', '<br><br><img src=', str_replace('(img url//)', '><br><br>', $exam->question)) !!}
-                </p>
-                <div class="question-choices-container">
-                    <ul>
-                        @if ($exam->option_a)
-                            <li class="multi-choice-item"><span class="multi-choice-letter"
-                                    data-choice-letter="A"></span>{{ $exam->option_a }}</li>
-                        @endif
-                        @if ($exam->option_b)
-                            <li class="multi-choice-item"><span class="multi-choice-letter"
-                                    data-choice-letter="B"></span>{{ $exam->option_b }}</li>
-                        @endif
-                        @if ($exam->option_c)
-                            <li class="multi-choice-item"><span class="multi-choice-letter"
-                                    data-choice-letter="C"></span>{{ $exam->option_c }}</li>
-                        @endif
-                        @if ($exam->option_d)
-                            <li class="multi-choice-item"><span class="multi-choice-letter"
-                                    data-choice-letter="D"></span>{{ $exam->option_d }}</li>
-                        @endif
-                        @if ($exam->option_e)
-                            <li class="multi-choice-item"><span class="multi-choice-letter"
-                                    data-choice-letter="E"></span>{{ $exam->option_e }}</li>
-                        @endif
-                        @if ($exam->option_f)
-                            <li class="multi-choice-item"><span class="multi-choice-letter"
-                                    data-choice-letter="F"></span>{{ $exam->option_f }}</li>
-                        @endif
-                    </ul>
-                </div><button class="toggle-answer-btn btn btn-primary">Show Answer</button>
-                <div class="card-text question-answer bg-light white-text hidden">
-                    <span class="vote-answer-box"><strong>Most Voted Answer:</strong><span
-                            class="vote-answer">{{ $exam->answer }}</span>
-                    </span><br><br><br><br>
+            <div class="container">
+                <div class="question">
+                    <div class="question-number">{{ $exam->question_number }}</div>
+                    <div class="question-text">
+                        <p class="card-text">{!! str_replace('(//img url)', '<br><br><img src=', str_replace('(img url//)', '><br><br>', $exam->question)) !!}
+                        </p>
+                    </div>
+                    <div class="question-options">
+                        <ul>
+                            @if ($exam->option_a)
+                                <li class="multi-choice-item"><span class="multi-choice-letter"
+                                        data-choice-letter="A"></span>{{ $exam->option_a }}</li>
+                            @endif
+                            @if ($exam->option_b)
+                                <li class="multi-choice-item"><span class="multi-choice-letter"
+                                        data-choice-letter="B"></span>{{ $exam->option_b }}</li>
+                            @endif
+                            @if ($exam->option_c)
+                                <li class="multi-choice-item"><span class="multi-choice-letter"
+                                        data-choice-letter="C"></span>{{ $exam->option_c }}</li>
+                            @endif
+                            @if ($exam->option_d)
+                                <li class="multi-choice-item"><span class="multi-choice-letter"
+                                        data-choice-letter="D"></span>{{ $exam->option_d }}</li>
+                            @endif
+                            @if ($exam->option_e)
+                                <li class="multi-choice-item"><span class="multi-choice-letter"
+                                        data-choice-letter="E"></span>{{ $exam->option_e }}</li>
+                            @endif
+                            @if ($exam->option_f)
+                                <li class="multi-choice-item"><span class="multi-choice-letter"
+                                        data-choice-letter="F"></span>{{ $exam->option_f }}</li>
+                            @endif
+                        </ul>
+                    </div>
+                </div>
+                <div class="buttons">
+                    <div class="show-answer"><button class="show-answer-btn" id="{{ $exam->answer }}">Show Answer</button></div>
+                    <div class="ask-gbt"><button class="ask-gbt-btn">Ask GBT</button></div>
                 </div>
             </div>
         @endforeach
-
     </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.show-answer-btn').on('click', function() {
+                var $questionContainer = $(this).closest('.container').find('.question'); // Find the closest question container
+                var correctAnswer = $(this).attr('id'); // Get the correct answer ID (A, B, C, etc.)
+
+                if ($(this).text() === 'Show Answer') {
+                    $(this).text('Hide Answer');
+
+                    // Find the corresponding choice item and highlight it within the current question container
+                    $questionContainer.find('.multi-choice-letter[data-choice-letter="' + correctAnswer + '"]').parent().addClass('correct-answer');
+                } else {
+                    $(this).text('Show Answer');
+
+                    // Remove highlighting from all options within the current question container
+                    $questionContainer.find('.multi-choice-item').removeClass('correct-answer');
+                }
+            });
+        });
+    </script>
 </x-layout>
